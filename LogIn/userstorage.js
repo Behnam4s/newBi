3bb92baec23fd6841433baf4207cbfe56d815c6e
Behnam <93790323+Behnam4s@users.noreply.github.com>
2023-02-18 12:19:28 +0330

    removeloginPage

--- LogIn/userstorage.js
+++
@@ -1,8 +0,0 @@
-let userData = document.getElementById('userData');
-userData.innerHTML = "کاربر گرامی " + localStorage.getItem("userName");
-userData.innerHTML += " با رمز عبور " + localStorage.getItem("passWord") + " خوش آمدید" ;
-function logOut(){
-    window.open("index.html","_self");
-    localStorage.removeItem("userName");
-    localStorage.removeItem("passWord");
-}
